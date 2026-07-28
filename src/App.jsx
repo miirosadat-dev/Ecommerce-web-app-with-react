ad5d8e96d36c05ac989de75565c6c588e55ccfdd
SADAT MIIRO <miirosadat0@gmail.com>
2026-07-28 17:58:18 +0300

    Organizing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { Header } from "./components/Header";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Category from "./components/Category";
+import Types from "./components/Types";
+import Services from "./components/Services";
+import Productsgrid from "./components/Productsgrid";
+import Banner from "./components/Banner";
+import Reviews from "./components/Reviews";
+import Insta from "./components/Insta";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Hero />
+      <Category />
+      <Types />
+      <Services />
+      <Productsgrid />
+      <Banner />
+      <Reviews />
+      <Insta />
+      <Footer />
     </>
   );
 }
