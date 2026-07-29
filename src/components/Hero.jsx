0409ed20705f52033128683dc94668ed1efc287f
SADAT MIIRO <miirosadat0@gmail.com>
2026-07-29 22:05:56 +0300

    Installing and configuring react slick

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import headset from "../assets/headset.png";
 
 const Hero = () => {
   return <div>Hero</div>;
