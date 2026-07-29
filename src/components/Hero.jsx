8fd49e972479e6f96c9604f58f62e125be53e4e6
SADAT MIIRO <miirosadat0@gmail.com>
2026-07-29 22:11:20 +0300

    import hero banner and aos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import headset from "../assets/headset.png";
+import headset from "../assets/headset.jpg";
+import earbuds from "../assets/earbuds.jpg";
+import dslr from "../assets/dslr.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Hero = () => {
   return <div>Hero</div>;
