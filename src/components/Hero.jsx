b3d1465881f5c8b18891fd02db06e5a292714c30
SADAT MIIRO <miirosadat0@gmail.com>
2026-07-29 22:18:21 +0300

    Setting react slick and AOS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,24 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Hero = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrowsL: false,
+  };
+
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: "ease-in-out",
+    });
+    AOS.refresh();
+  }, []);
+
   return <div>Hero</div>;
 };
 
