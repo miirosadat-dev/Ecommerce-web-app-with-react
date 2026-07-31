df98596783a17085150a2f010305a924be94b1e0
SADAT MIIRO <miirosadat0@gmail.com>
2026-07-31 12:48:09 +0300

    Setting up services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { useEffect } from "react";
+import payment from "../assets/payment.png";
+import shipping from "../assets/shipping.png";
+import refund from "../assets/return.png";
+import gift from "../assets/gift.png";
 
 const Services = () => {
   return <div>Services</div>;
